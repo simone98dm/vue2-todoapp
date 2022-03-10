f9debda0b1aa4931b612fd61590188f2e67de5a1
simone98dm <simone.dalmas@outlook.it>
2022-03-10 19:41:35 +0100

    fix color

--- src/components/Button.vue
+++ src/components/Button.vue
@@ -1,17 +1,6 @@
 <template>
   <button
-    v-bind:class="`
-    bg-${variant}-600 
-    text-white 
-    px-4 
-    py-2 
-    m-2
-    rounded-md 
-    text-1xl 
-    font-medium 
-    hover:bg-${variant}-800 
-    transition 
-    duration-300`"
+    v-bind:class="`bg-purple-600 text-white px-4 py-2 m-2 rounded-md text-1xl font-medium hover:bg-purple-800 transition duration-300`"
     v-on:click="this.clickEvent"
   >
     {{ this.text }}
