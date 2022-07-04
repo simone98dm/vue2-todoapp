88b45d407897dd69eed876f4429d0f7d41a5651b
Simone <simone.dalmas@outlook.it>
2022-07-04 13:22:45 +0200

    fix wrong path

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/vue2-proj/'
+    ? '/vue2-todoapp/'
     : '/'
 })
